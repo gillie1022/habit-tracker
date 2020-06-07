d45902b37ce9f456b5b82bde5568d30741b1e16c
gillie1022 <dave.gillie@icloud.com>
2020-06-07 11:23:34 -0400

    cleaned up JS by using defer instead of DOMContentLoaded

--- static/js/habit.js
+++ static/js/habit.js
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document
-    .querySelector("#show_habit_form")
-    .addEventListener("click", (event) => {
-      event.preventDefault();
-      document.querySelector("#record_form").classList.remove("dn");
-    });
-});
+document
+  .querySelector("#show_habit_form")
+  .addEventListener("click", (event) => {
+    event.preventDefault();
+    document.querySelector("#record_form").classList.remove("dn");
+  });
