fb5d7390e8f35339b309ae4afb25c4a1802bbf2c
gillie1022 <dave.gillie@icloud.com>
2020-06-07 19:39:30 -0400

    restarted my computer, renamed the JS file, and now it works

--- static/js/habit.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener(DOMContentLoaded){
-
-    document.querySelector("#show-record-form").addEventListener("click", (event) => {event.preventDefault();
-        document.querySelector("#record-form").classList.remove("dn");
-        document.querySelector("#show-record-form").classList.add("dn")
-        document.querySelector("#id_qauntity").focus()
-    })
-}
---
+++ static/js/habit_tracker.js
@@ -0,0 +1,5 @@
+document.querySelector("#show-record-form").addEventListener("click", (event) => {event.preventDefault();
+    document.querySelector("#record-form").classList.remove("dn");
+    document.querySelector("#show-record-form").classList.add("dn")
+    document.querySelector("#id_quantity").focus()
+})
