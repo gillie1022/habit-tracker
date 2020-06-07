8a886ee0c5a32f3ed1faeb1bbc5216f52a3cec1d
gillie1022 <dave.gillie@icloud.com>
2020-06-07 19:30:01 -0400

    digging a hole in JS

--- static/js/habit.js
+++ static/js/habit.js
@@ -1,5 +1,8 @@
-document.querySelector("#show-record-form".addEventListener("submit", (event) => {event.preventDefault();
-    document.querySelector("#record_form").classList.remove("dn");
-    document.querySelector("#show-record-form").classList.add("dn")
-    document.querySelector("#id_qauntity").focus()
-  });
+document.addEventListener(DOMContentLoaded){
+
+    document.querySelector("#show-record-form").addEventListener("click", (event) => {event.preventDefault();
+        document.querySelector("#record-form").classList.remove("dn");
+        document.querySelector("#show-record-form").classList.add("dn")
+        document.querySelector("#id_qauntity").focus()
+    })
+}
