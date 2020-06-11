3efa75dcc42a1331a7994186efa99592b7b7ec36
gillie1022 <dave.gillie@icloud.com>
2020-06-10 21:53:38 -0400

    some styling in place, lots more to do

--- static/js/tracker.js
+++ static/js/tracker.js
@@ -1,12 +1,3 @@
-// document
-//   .querySelector("#show-record-form")
-//   .addEventListener("click", (event) => {
-//     event.preventDefault();
-//     document.querySelector("#record-form").classList.remove("dn");
-//     document.querySelector("#show-record-form").classList.add("dn");
-//     document.querySelector("#id_quantity").focus();
-//   });
-
 records = document.querySelectorAll(".record");
 for (let record of records) {
   record.addEventListener("click", (event) => {
